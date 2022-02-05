738063cd1cfbf927824ad21996a313fd8fb7279f
Christopher Jurnak <85128143+Jurnak24@users.noreply.github.com>
2022-02-04 23:05:03 -0500

    Add files via upload

---
+++ redux/configureStore.js
@@ -0,0 +1,26 @@
+import {createStore, combineReducers} from 'redux';
+import { Campsites } from './campsites';
+import { Comments } from './comments';
+import { Partners } from './partners';
+import { Promotions } from './promotions';
+import {createStore, combineReducers, applyMiddleware } from 'redux';
+import { createForms } from 'react-redux-form';
+import { InitialFeedback } from './forms';
+
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+export const ConfigureStore = () => {
+    const store = createStore(
+        combineReducers({
+            campsites: Campsites,
+            comments: Comments,
+            partners: Partners,
+            promotions: Promotions,
+            ...createForms({
+                feedbackForm: InitialFeedback
+            })
+        }),
+
+    return store;
+};
